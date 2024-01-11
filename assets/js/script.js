1b6ffeb1b881abbbe7cad90eeca054bd7e2c8493
Nikhil Tembhare <odincontactme@gmail.com>
2024-01-11 08:59:15 +0530

    Services & Work
    Services And Work Sections Added

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
+
+/*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalClose = document.querySelectorAll(".services__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((mb, i) => {
+  mb.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalClose.forEach((mc) => {
+  mc.addEventListener("click", () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove("active-modal");
+    });
+  });
+});
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+var mixerPortfolio = mixitup(".work__container", {
+  selectors: {
+    target: ".work__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
+
+/* Link active work */
+const linkWork = document.querySelectorAll(".work__item");
+
+function activeWork() {
+  linkWork.forEach((L) => L.classList.remove("active-work"));
+  this.classList.add("active-work");
+}
+
+linkWork.forEach((L) => L.addEventListener("click", activeWork));
